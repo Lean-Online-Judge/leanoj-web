9040e9f3c2706cfebd3a75f536918367dfbb4a5e
ansar <azhdarov.ansar@gmail.com>
2026-02-22 18:08:24 +0100

    links links

--- templates/guide.php
+++ templates/guide.php
@@ -17,12 +17,11 @@ Replace <code>sorry</code> in the problem template with your proof and submit th
 <li>
 <p>
 <b>Problems that ask for an answer.</b><br>
-Some problems ask to provide an answer and prove that it's correct. Formally defining what an acceptable answer should be is difficult. Instead, valid answers are predefined and added to the <a target="_blank" href="index.php?action=view_answers">Answer Bank</a>. Replace the <code>answer</code> declaration in the problem template with one from the Answer Bank, then prove its correctness as usual.
+Some problems ask to provide an answer and prove that it's correct. Formally defining what an acceptable answer should be is difficult. Instead, valid answers are predefined and added to the <a href="index.php?action=view_answers">Answer Bank</a>. Replace the <code>answer</code> declaration in the problem template with one from the Answer Bank, then prove its correctness as usual.
 </p>
-
 <li>
 <p>
-<b><a target="_blank" href="index.php?action=view_problem&id=25">xyzzy</a>.</b><br>
+<b><a href="index.php?action=view_problem&id=25">xyzzy</a>.</b><br>
 This is a special problem that doesn't have a real template and can be used to submit a solution to any custom problem. It can also be used to test the checker against potential exploits (please, create an issue on <a target="_blank" href="https://github.com/Lean-Online-Judge/leanoj-checker/issues">GitHub</a> if you find one). Submissions are not visible to other participants but still visible to the admin.
 </p>
 </ul>
