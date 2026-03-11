0a3aadc6c69ca9c80c56ff9ade3541a9d3376a4f
ansar <azhdarov.ansar@gmail.com>
2026-03-11 05:34:47 +0100

    status info updated

--- templates/status_info.php
+++ templates/status_info.php
@@ -1,62 +1,42 @@
 <h2>Submission Status</h2>
 <p>
-  Below is a brief explanation of possible submission statuses. For details, see the checker <a target="_blank" href="https://github.com/Lean-Online-Judge/leanoj-checker">source code</a>.
+  Below is a brief explanation of possible submission statuses. For details, see the checker <a target="_blank" href="https://github.com/Lean-Online-Judge/leanoj-web/blob/master/worker.php">source code</a>.
+
 </p>
-<ul>
-  <li>
-  <p>
-    <span class="status-pending">PENDING</span><br>
-    The submission has not been judged yet. You may need to refresh the page to see if the status has been updated.
-  </p>
-  <li>
-  <p>
-    <span class="status-passed">PASSED</span><br>
-    The submission has passed. Hooray!
-  </p>
-  <li>
-  <p>
-    <span class="status-cell">Blame author</span><br>
-    The author has made a mistake when preparing the problem (or it appeared after a <code>Mathlib</code> upgrade). Please, <a target="_blank" href="https://discord.gg/a4xYPXXBxU">contact</a> the admin if you encounter this.
-  </p>
-  <li>
-  <p>
-    <span class="status-cell">Compilation Error</span><br>
-    The submission compilation has failed. Make sure your <code>Mathlib</code> version matches the one used by the checker. It is normally the latest stable one (currently, v4.28.0).
-  </p>
-  <li>
-  <p>
-    <span class="status-cell">Environment error</span><br>
-    This error is caused by attempts to modify the Lean environment via metaprogramming (presumably, to trick the checker). If that is your intention, there is a special <a href="index.php?action=view_problem&id=25">problem</a> for that. Some tactics may also cause this error (e.g., <code>native_decide</code>).
-  </p>
-  <li>
-  <p>
-    <span class="status-cell">Forbidden axiom</span><br>
-    The solution uses axioms outside of <code>propext</code>, <code>Classical.choice</code>, and <code>Quot.sound</code>. Having unfilled <code>sorry</code> also causes this error (because it uses <code>sorryAx</code> under the hood). You can view the axioms used with the <code>#print axioms</code> macro in your Lean editor.
-  </p>
-  <li>
-  <p>
-    <span class="status-cell">Template mismatch</span><br>
-    Some declarations in the template are missing or do not match those in the submission (up to definitional equality). Submissions can still use extra declarations.
-  </p>
-  <li>
-  <p>
-    <span class="status-cell">Bad answer</span><br>
-    The provided answer is not (definitionally) equal to the one set for this problem. Make sure to use an answer from the <a href="index.php?action=view_answers">Answer Bank</a>.
-  </p>
-  <li>
-  <p>
-    <span class="status-cell">Time out</span><br>
-    Judging the submission took more time than the predefined limit (generous 300 seconds). This might be caused by heavy imports (e.g., whole <code>Mathlib</code>). The <code>#min_imports</code> macro can be helpful to slim them down.
-  </p>
-  <li>
-  <p>
-    <span class="status-cell">Out of memory</span><br>
-    Judging the submission took more memory than the predefined limit (around 4 gigabytes).
-  </p>
-  <li>
-  <p>
-    <span class="status-cell">Unknown error</span><br>
-    The cause of the error is unknown. Please, <a target="_blank" href="https://discord.gg/a4xYPXXBxU">contact</a> the admin if you encounter this.
-  </p>
-  </p>
-</ul>
+<h3>Pending</h3>
+<p>
+  The submission has not been judged yet. You may need to refresh the page to see if the status has been updated.
+</p>
+<h3>System error</h3>
+<p>
+  Something went wrong on the system side. Please, contact the admin via <a target="_blank" href="https://discord.gg/a4xYPXXBxU">Discord</a>.
+</p>
+<h3>Compilation Error</h3>
+</p>
+  Submission compilation has failed. Make sure your <code>Mathlib</code> version matches the one used by the checker. It is normally the latest stable (currently, v4.28.0). You can check the version in your Lean editor with <code>#eval Lean.versionString</code>.
+</p>
+<h3>Time out</h3>
+<p>
+  Submission compilation took more time than the predefined limit. This might be caused by heavy imports (e.g., whole <code>Mathlib</code>). The <code>#min_imports</code> macro can be helpful in this case.
+</p>
+<h3>Out of memory</h3>
+<p>
+  Submission compilation took more memory than the predefined limit.
+</p>
+<h3>Bad answer</h3>
+<p>
+  The provided answer does not match one set for this problem. Make sure you are using one from the <a href="index.php?action=view_answers">Answer Bank</a>.
+</p>
+<h3>Template mismatch</h3>
+<p>
+  Some declarations in the template are missing or do not match those in the submission. Extra declarations are allowed.
+</p>
+<h3>Rejected</h3>
+<p>
+  The submission has not passed extra verification. Make sure that <code>propext</code>, <code>Classical.choice</code>, and <code>Quot.sound</code> are the only axioms used. This can be checked with <code>#print axioms solution</code>. Note that some tactics, such as <code>native_decide</code>, may rely on extra axioms. The status can also be caused by attempts to trick the checker (e.g., by tampering the environment via metaprogramming). If that is your intention, there is a special <a href="index.php?action=view_problem&id=25">problem</a> for that.
+
+</p>
+<h3>Passed</h3>
+<p>
+  The submission has passed. Hooray!
+</p>
